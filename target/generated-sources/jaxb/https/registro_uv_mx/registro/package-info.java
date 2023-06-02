8ba6068f3a1abe29f59f7584e0aaaf7bc7e5298d
JiroFg <mechcromancer@gmail.com>
2023-06-02 01:43:58 -0600

    fixed

--- target/generated-sources/jaxb/https/registro_uv_mx/registro/package-info.java
+++ target/generated-sources/jaxb/https/registro_uv_mx/registro/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.06.02 a las 01:34:25 AM CST 
+// Generado el: 2023.06.02 a las 02:42:16 AM CDT 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "https://registro.uv.mx/registro", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
